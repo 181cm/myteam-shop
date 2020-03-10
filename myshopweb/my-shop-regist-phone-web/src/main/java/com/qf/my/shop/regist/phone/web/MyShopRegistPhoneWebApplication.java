f1443b0ee2937dc964af518fd356a0b721474ecc
shizelei <214490523@qq.com>
2020-03-10 09:37:28 +0800

    regist phone web

---
+++ myshopweb/my-shop-regist-phone-web/src/main/java/com/qf/my/shop/regist/phone/web/MyShopRegistPhoneWebApplication.java
@@ -0,0 +1,13 @@
+package com.qf.my.shop.regist.phone.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyShopRegistPhoneWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyShopRegistPhoneWebApplication.class, args);
+    }
+
+}
---
+++ myshopweb/my-shop-regist-phone-web/src/test/java/com/qf/my/shop/regist/phone/web/MyShopRegistPhoneWebApplicationTests.java
@@ -0,0 +1,16 @@
+package com.qf.my.shop.regist.phone.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyShopRegistPhoneWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+
+        System.out.println("hello git");
+
+    }
+
+}
